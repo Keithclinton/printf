f9459fd665fc53fd5789e337937d85dbd1b74453
kyaloMaema <brianmaema28@gmail.com>
2023-11-14 08:56:50 +0300

    Update main.h
    Corrected changes

--- main.h
+++ main.h
@@ -74,7 +74,7 @@ char buffer[], int flags, char flag_ch, int width, int precision, int size);
 int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/* Funcion to print memory address */
+/* Funcion to print the memory address */
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
